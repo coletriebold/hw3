cd3c2f2e743566c0cde84eb3c23760c3790ebd60
coletriebold <35664168+coletriebold@users.noreply.github.com>
2018-02-05 16:15:44 -0800

    Create drawing-new-colors.js

---
+++ drawing-new-colors.js
@@ -0,0 +1,31 @@
+function setup() { 
+  createCanvas(400, 400);
+} 
+
+var diameter = 10;
+
+function draw() { 
+  if (mouseIsPressed) {
+    ellipse(mouseX, mouseY, diameter);
+  }
+}
+
+function keyPressed() {
+  print(key);
+  if (key == 'R') {
+    fill(255, 20, 20);
+  } else if (key == 'G') {
+    fill(20, 255, 20);
+  } else if (key == 'B') {
+    fill(20, 20, 255); 
+  }
+	else if (key == 'W') {
+    fill(0, 0, 0);
+	}
+	else if (key == 'P') {
+    fill(128,0,128);
+	}
+	else if (key == 'Q') {
+    fill(random(255),random(255),random(255));
+	}
+}
